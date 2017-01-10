4614d31ab978be79e45e43c4876fe7031b0e5e37
vecin2 <vecin2@gmail.com>
2017-01-10 15:05:45 +0000

    create rebuild environment which joins recreate environemnt and rebuild data

--- rebuild-environment-data.sh
+++ rebuild-environment-data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ENV_NAME=localhost
+ENV_NAME=LOCAL_AD
 CONTAINER_NAME=localhost
 CORE_HOME=/opt/em/projects/prudential/MCCS/AgentDesktop/
 $CORE_HOME/bin/ccadmin.sh stop-all-appservers -Denvironment.name=$ENV_NAME -Dmachine.name=localhost
@@ -10,10 +10,6 @@ $CORE_HOME/bin/ccadmin.sh stop-all-appservers -Denvironment.name=$ENV_NAME -Dmac
 $CORE_HOME/bin/ccadmin.sh rebuild-database -Denvironment.name=$ENV_NAME -Dmachine.name=localhost
 $CORE_HOME/bin/ccadmin.sh load-workflows -Denvironment.name=$ENV_NAME -Dmachine.name=localhost
 
-#importing data expect kmconfig because it is broken
-$CORE_HOME/bin/ccadmin.sh import-data -Denvironment.name=$ENV_NAME -Dmachine.name=localhost -Dcontainer.name=$CONTAINER_NAME -DimportLocation=$CORE_HOME/project/resources/migration/Content.jar
-#$CORE_HOME/bin/ccadmin.sh import-data -Denvironment.name=$ENV_NAME -Dmachine.name=localhost -Dcontainer.name=$CONTAINER_NAME -DimportLocation=$CORE_HOME/project/resources/migration/KMConfig.jar
-$CORE_HOME/bin/ccadmin.sh import-data -Denvironment.name=$ENV_NAME -Dmachine.name=localhost -Dcontainer.name=$CONTAINER_NAME -DimportLocation=$CORE_HOME/project/resources/migration/Rules.jar
-$CORE_HOME/bin/ccadmin.sh import-data -Denvironment.name=$ENV_NAME -Dmachine.name=localhost -Dcontainer.name=$CONTAINER_NAME -DimportLocation=$CORE_HOME/project/resources/migration/ReleaseMigration.jar
-
+#importing data 
+./import-data.sh
 
