d50059e44f2c8c7d0a4a3033106f25628b8d19ff
vecin2 <vecin2@gmail.com>
2017-04-23 09:30:15 +0100

    addgin mara and sbank tags

---
+++ mara-sql-tag.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+. ./sql-base.sh
+. ./sql-tag-base.sh
+
+SQL_MODULE=/SGroupContactHistory/sqlScripts/oracle/updates/SG0_3/AddMaRaContactReasonTag
+REVISION=460
+
+#MM/DD/YYYY HH:MI:SS AM
+DATE="04/21/2017"
+
+
+
+declare -a DISPLAY_NAMES=('MaRa Contact Reasons' 
+					'['
+					   'Asiakas'
+					   '[' 
+						'Uusi varaus'
+						'Verma-varaus'
+						'Varauksen muutos'
+						'Peruutus'
+						'Internettuki'
+						'Joku muu/ Muu asiakkaan neuvonta'
+						'Ennakkolasku'
+						'Myöhäisen tuloajan ilmoitus'
+						'Nettikampanja  (Black Friday tms)'
+						'Reklamaatio'
+						'Tarjouspyyntö/hintatiedustelu'
+						'Spam/Out of office/Ei tarvitse vastata'
+						'Laskutuslupakysely'
+						'Pöytävaraus'
+						'Ryhmä-, kokous tai ravintolapuhelu'
+						'Reconfirmation'
+					    ']' 
+                                            'Myyntipäällikkö'
+					    '['
+						'Uusi varauspyyntö'
+						'Muutos olemassa olevaan varaukseen'
+						'Peruutus'
+						'Neuvontaa'
+						'Tarkistus tiedetäänkö ajankohtaisista kamppiksista ym'
+						'Spam/Out of office/Ei tarvitse vastata'
+						'Joku muu'
+					    ']'
+					    'Yksikkö'
+					    '['
+						'Soitto mypan soittopyynnön perään'
+						'Menneen tai tulevan varauksen selvityspyyntö'
+						'Uusi varaus'
+						'Muutos olemassa olevaan varaukseen'
+						'Peruutus'
+						'Reklamaatio'
+						'Joku muu'
+					    ']'
+					    'PK/TRE mypa'
+					    '['
+						'Aiheellinen kysely'
+						'Aiheeton kysely (itse olisi voinut hoitaa)'
+					    ']'
+					    'Tukihenkilö'
+					    '['
+					    	'Avunpyyntö'
+					    	'Selvitys'
+					    	'Reklamaatio'
+					    ']'
+					    'Esimies'
+					    '['
+					    	'Selvitys'
+					    	'Reklamaatio'
+					    ']'
+					 ']')
+SQL=$(generate_tag)
+echo "$SQL"
+create_sql_module $SQL_MODULE $REVISION "$SQL"
+
