09ac0e3eb11b0e8ec0968957f0d090dbb2828082
vecin2 <vecin2@gmail.com>
2017-07-26 15:02:46 +0100

    if error visualize it

--- migration-reimport-caseType-jar.sh
+++ migration-reimport-caseType-jar.sh
@@ -7,5 +7,14 @@ svn up
 echo "Changing directory back to $SHELL_DIR"
 cd $SHELL_DIR 
 
-./ccadmin.sh import-data -DimportLocation=/opt/verint/verint_projects/SPEN/FP3/project/migration-jar/migration-3PDCaseType.jar
+RESULT=$(./ccadmin.sh import-data -DimportLocation=/opt/verint/verint_projects/SPEN/FP3/project/migration-jar/migration-3PDCaseType.jar)
+SESSION_LINE=$(echo $RESULT | grep "Please check the session and migration logs for session '")
+SESSION_ID=$(echo $SESSION_LINE |  awk '{for(i=1;i<=NF;i++) if ($i=="for" && $(i+1)=="session") print $(i+2)}' | sed "s/'//g")
 
+LOG=$ADPROCESSLOGS/../migration/migration_$SESSION_ID.log
+
+echo log path is $LOG
+
+if [ "$SESSION_ID" ]; then
+	vi $LOG
+fi
