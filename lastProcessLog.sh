8137e8ea44dfd503d7d94071181b639e6060598e
vecin2 <vecin2@gmail.com>
2017-07-27 12:00:28 +0100

    updating to use profile variable

--- lastProcessLog.sh
+++ lastProcessLog.sh
@@ -9,10 +9,9 @@ else
 fi
 last_log_path(){
 
-	SESSION_LOGS_PATH=../logs/localhost-container_ad_1/cre/session/process/
-	FILE_NAME=$(ls $SESSION_LOGS_PATH -ltr | grep processLog | tail -$1 | head -1 | rev | cut -d " " -f1 | rev)
+	FILE_NAME=$(ls $ADPROCESSLOGS -ltr | grep processLog | tail -$1 | head -1 | rev | cut -d " " -f1 | rev)
 
-	FULL_PATH=$SESSION_LOGS_PATH/$FILE_NAME
+	FULL_PATH=$ADPROCESSLOGS/$FILE_NAME
 	echo $FULL_PATH
 }
 
