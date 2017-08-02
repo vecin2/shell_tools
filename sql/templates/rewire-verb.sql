105f4f37e22b989e48635c2fe05e93d1885cbfe2
vecin2 <vecin2@gmail.com>
2017-08-02 12:46:17 +0100

    adding space

--- sql/templates/rewire-verb.sql
+++ sql/templates/rewire-verb.sql
@@ -1,5 +1,5 @@
 
 update eva_verb 
 set (PROCESS_DESC_REF_ID) = (@PDR.$PROCESS_DESC_REF_ID)
-where ENTITY_DEF_ID =@ED.$ENTITY_DEF_ID and name ='$VERB_NAME';
+where ENTITY_DEF_ID = @ED.$ENTITY_DEF_ID and name ='$VERB_NAME';
 
