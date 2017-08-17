a421ae1324e332c055cde5e6e90bc78486907d78
vecin2 <vecin2@gmail.com>
2017-08-17 08:17:57 +0100

    compute unit test path from object class

--- xml/repository.sh
+++ xml/repository.sh
@@ -3,6 +3,24 @@
 unit_test_path(){
 	obj_path=$1
 	module=$(echo $obj_path | awk -F "/Implementation" '{print $1}')
-	result=$module/Test/xml/UnitTests
+	counter=0
+	imp_subpackage=$(imp_subpackage $obj_path)
+	result=$module/Test/$imp_subpackage/UnitTests
 	echo $result
+}
+imp_subpackage(){
+	index_of_implementation $1
+	implementation_index=$?
+	let "cut_index=implementation_index +2"
+	echo $obj_path | cut -d '/' -f$cut_index
+}
+index_of_implementation(){
+	obj_path=$1
+	for folder in ${obj_path//\// }
+	do
+		if [ "$folder" == "Implementation" ]; then
+			return $counter
+		fi
+		let "counter++"
+	done
 }
