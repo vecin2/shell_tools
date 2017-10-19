8271ac0ddd11a624710ca9dcbc2087477735baf8
vecin2 <vecin2@gmail.com>
2017-10-19 15:39:42 +0100

    Fixing issue when stealing locks

--- svn-ci.sh
+++ svn-ci.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$#" -eq 1 ]; 
-then
+if [ "$#" -eq 1 ]; then
 	echo No repository path enter, using project specific modules
-elif [ "$#" -ne 2 ];
-then
+elif [ "$#" -ne 2 ]; then
 	echo Usage commit.message ="a commit message must be entered", repo.path="the path within repository to look for commit (optional, its default to project specific modules)"
 	return 1
 fi
 
-./svn-status.sh $2 | grep -v \? | awk '{print $2}' | xargs echo svn commit -m \"$1\"  | bash
+
+
+./svn-status.sh $2 | grep -v \? | awk '{print $NF}' | xargs echo svn commit -m \"$1\" | bash
