ad32d7a104944c6060e7412fb72defade2f1257a
vecin2 <vecin2@gmail.com>
2020-02-11 21:24:51 -0800

    generating ccadmin options when installing autocomplete

--- autocompletion/install-autocompletion.sh
+++ autocompletion/install-autocompletion.sh
@@ -5,3 +5,11 @@ else
 	sudo ln -sfv $AD/shell_tools/autocompletion/ccadmin.sh /etc/bash_completion.d/ccadmin
 	sudo ln -sfv $AD/shell_tools/autocompletion/gt.sh /etc/bash_completion.d/gt
 fi 
+completion_file_location="${EM_CORE_HOME}/.em/autocompletion/ccadmin_completion"
+if [ ! -f "$completion_file_location" ]; then
+	echo "Generating ccadmin options"...
+	options=$("${EM_CORE_HOME}"/bin/ccadmin.sh | grep echo | awk '{print $3}')
+	echo $options
+	mkdir -p "${EM_CORE_HOME}/.em/autocompletion"
+	echo "$options"	>"$completion_file_location"
+fi
